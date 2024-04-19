f43ceadc870253f1622a58f2a7bf0fd00e4f5551
gabb-z <gabbz.liima@gmail.com>
2024-04-19 16:58:40 -0300

    Projeto 04 - Aumento Salarial

---
+++ Poo004/Poo004/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo004
+{
+    class Funcionario
+    {
+        //Atributos da Classe
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        //Função da Classe - Salário Líquido
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        //Função da Classe - Aumentar Salário
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += (SalarioBruto * porcentagem) / 100.0;
+        }
+
+        //Override - ToString
+        public override string ToString()
+        {
+            return Nome + ", R$ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Poo004/Poo004/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de Objeto - Funcionário
+            Funcionario func = new Funcionario();
+
+            //Entrada de Dados - Dados do Funcionário 
+            Console.WriteLine("Digite o(s) dado(s) do(a) funcionário(a): ");
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: R$ ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: R$ ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Dados do Funcionário
+            Console.WriteLine("\nDado(s) do(a) funcionário(a): " + func);
+
+            //Entrada de Dados - Aum. Salário
+            Console.Write("\nDigite a porcentagem que deseja aumentara o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcentagem);
+
+            //Dados do Funcionário - Atualizados
+            Console.WriteLine("\nDado(s) atualizado(s): " + func);
+        }
+    }
+}
