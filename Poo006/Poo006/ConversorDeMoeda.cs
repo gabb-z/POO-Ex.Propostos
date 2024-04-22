495706c68720c69db4ca855da94fa7ea125c1bc9
gabb-z <gabbz.liima@gmail.com>
2024-04-22 12:18:20 -0300

    Projeto 06 - Calculadora: Dólar para Real

---
+++ Poo006/Poo006/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo006
+{
+    class ConversorDeMoeda
+    {
+        //Atributo Estático - Iof 6%
+        public static double Iof = 6.0;
+
+        //Função Estática de Conversão - Dólar para Real
+        public static double DolarParaReal(double cotacao, double qtde)
+        {
+            double total = cotacao * qtde;
+            return total + total * Iof / 100.0;
+        }
+    }
+}
---
+++ Poo006/Poo006/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada de Dados - Cotação & Compra de Dólar
+            Console.Write("Digite a cotação atual do dólar: R$ ");
+            double cotação = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite a quantidade que deseja comprar em dólar: $ ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Saída de Dados - Valor à pagar em R$
+            Console.WriteLine("\nValor à ser pago em reais: R$ " + ConversorDeMoeda.DolarParaReal(cotação, quantidade).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
