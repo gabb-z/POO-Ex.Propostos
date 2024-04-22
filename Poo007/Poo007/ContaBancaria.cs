df28df0bbc9ab9791721fb8f4dc811c02df6e018
gabb-z <gabbz.liima@gmail.com>
2024-04-22 12:57:04 -0300

    Projeto 07 - Conta Bancária

---
+++ Poo007/Poo007/ContaBancaria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo007
+{
+    class ContaBancaria
+    {
+        //Atributos da Classe
+        public int NumeroConta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        //Construtores da Classe
+        public ContaBancaria()
+        {
+
+        }
+        public ContaBancaria(int numeroConta, string titular)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+        }
+        public ContaBancaria(int numeroConta, string titular, double saldo) : this(numeroConta, titular)
+        {
+            Saldo = saldo;
+        }
+
+        //Função da Classe - Depósito
+        public double Deposito(double valor)
+        {
+            return Saldo += valor;
+        }
+
+        //Função da Classe - Saque / Taxa de 5% Sobre Saque
+        public double Saque(double valor)
+        {
+            return Saldo -= valor + 5.0;
+        }
+
+        //Override - Objeto para String
+        public override string ToString()
+        {
+            return "Conta: " + NumeroConta + ", Titular: " + Titular + ", Saldo: R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture); 
+        }
+    }
+}
---
+++ Poo007/Poo007/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de Objeto - Conta Bancária
+            ContaBancaria conta = new ContaBancaria();
+
+            //Entrada de Dados - Dados do Titular
+            Console.Write("Digite o nº da conta: ");
+            int nConta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do(a) titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+
+            //Entrada de Dados - Haverá Depósito Inicial?
+            Console.Write("Deseja realizar um depósito inicial? (s/n): ");
+            char opcao = char.Parse(Console.ReadLine());
+
+            //Condicional - Depósito Inicial
+            if (opcao == 's' || opcao == 'S')
+            {
+                Console.Write("Digite o valor do depósito inicial: R$ ");
+                double depInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(nConta, nomeTitular, depInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(nConta, nomeTitular);
+            }
+
+            //Saída de Dados - Criacação de Conta
+            Console.WriteLine("\nDado(s) da conta: " + conta);
+
+            //Entrada de Dados - Realizar Depósito
+            Console.Write("\nDigite um valor para depósito: R$ ");
+            double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(valorDeposito);
+
+            //Saída de Dados - Atualizado
+            Console.WriteLine("\nDado(s) da conta atualizado(s): " + conta);
+
+            //Entrada de Dados - Realizar Saque
+            Console.Write("\nDigite um valor para saque: R$ ");
+            double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(valorSaque);
+
+            //Saída de Dados - Atualizado
+            Console.WriteLine("\nDado(s) atualizado(s): " + conta);
+        }
+    }
+}
