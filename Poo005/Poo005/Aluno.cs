b1d39201d01a833088046cd4ff712c66d6eaacc5
gabb-z <gabbz.liima@gmail.com>
2024-04-22 12:11:54 -0300

    Projeto 05 - Aprovado ou Reprovado

---
+++ Poo005/Poo005/Aluno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo005
+{
+    class Aluno
+    {
+        //Atributos da Classe
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        //Função da Classe - Nota Final
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        //Condicional - Aprovado ou Reprovado
+        public bool Aprovado()
+        {
+            if (NotaFinal() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Condicional - Nota Restante se Reprovado
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - NotaFinal();
+            }
+        }
+    }
+}
---
+++ Poo005/Poo005/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Poo005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de Objeto - Aluno
+            Aluno aluno = new Aluno();
+
+            //Entrada de Dados - Nome & Notas do Aluno
+            Console.WriteLine("Digite os dados do(a) aluno(a): ");
+            Console.Write("Nome do(a) aluno(a): ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("\nDigite as três notas do(a) aluno(a): ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Saída de Dados - Nota Final
+            Console.WriteLine("\nNota final: " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            //Condicional - Aprovado ou Reprovado
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("\nAPROVADO!");
+            }
+            else
+            {
+                Console.WriteLine("\nREPROVADO!");
+                Console.WriteLine("FALTARAM " + aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS!");
+            }
+        }
+    }
+}
